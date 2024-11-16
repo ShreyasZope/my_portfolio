ae0c2d2fad0c7d1bcde6f6dbd6b54dedfd1045a4
ShreyasZope <130879543+ShreyasZope@users.noreply.github.com>
2024-11-16 19:37:44 +0530

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Secur Social, a Social Media Platform on Blockchain Techonology (Prototype)",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A Proper use of development tools to develop a web application based of blockchain integration",
+    url: "https://github.com/ShreyasZope/Secure_Social",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development for Beginners (MERN Project)",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A simple MERN web application that helps students check their eligibility to get into collage.",
+    url: "https://github.com/ShreyasZope/My_Web_dev_Project",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://shreyaszope.github.io/Resume2/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Image Store(Pet-Project)",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A Web Application that lets you upload your images on the website where the website stores the images with integration of MongoDB.",
+    url: "https://youtu.be/VfiUA_S9E_g?si=KsM-GDsm6D2NDg_s",
   },
 ];
 
