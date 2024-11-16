646acb3bc190682bc2f7c90071eb75507bda73ee
ShreyasZope <130879543+ShreyasZope@users.noreply.github.com>
2024-11-16 19:35:41 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a student in Sinhgad Institute of Technology and i enjoy working on backends of the webpages";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Backend development",
+  "Restful API development",
+  "Python Language",
+  "Problem Slving Skills",
+  "Data Structures and Alogoithums",
+  "Database Integration",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. I like the challenge form the programming problems which drives me to enhance my logical thinking.";
 
 const About = () => {
   return (
